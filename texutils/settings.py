b102f7e4dad2075ba8008eab17537c6fda118e58
alphaboy198 <67190463+alphaboy198@users.noreply.github.com>
2021-05-26 00:42:26 -0700

    b

--- texutils/settings.py
+++ texutils/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$i=ci)@ov0=sz=_jfb$%ir@ftklw^5p_-g-uk6m+-@116w=v^m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','abdurrabtextutils.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1',abdurrab.herokuapp.com/']
 
 # Application definition
 
